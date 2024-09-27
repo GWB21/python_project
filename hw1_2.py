c0700388c17a8a91bd30ba5b95df0bb0c40060f8
Victor <yellower21@cau.ac.kr>
2024-09-27 13:42:34 +0900

    hw1 problem1

--- hw1_2.py
+++ hw1_2.py
@@ -1,1 +1,20 @@
-hello
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+def three_way_merge_sort(arr):
+    i = j = k = 0
+    merged_arr = []
+    left = arr[:len(arr)//3]
+    right = arr[(len(arr)//3) * 2:]
+    middle = arr[len(arr)//3 : (len(arr)//3) * 2]
+
+arr = [1,6,34,8,345,7,9,567,777, 999]
+print(quick_sort(arr))
+print(three_way_merge_sort(quick_sort(arr)))
--- prac.py
+++ prac.py
@@ -1,41 +1,4 @@
-a = [1, 17, 47, 67, 88]
-b = [3 , 12, 36, 59, 66]
-c = [ 3, 1234, 4, 1, 46]
-def merge_sort(arr1, arr2):
-    i = 0
-    j = 0
-    merged_arr =[]
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            merged_arr.append(arr1[i])
-            i += 1
-        else:
-            merged_arr.append(arr2[j])
-            j += 1
-
-    # while i < len(arr1):
-    #     merged_arr.append(arr1[i])
-    #     i += 1
-    #
-    # while j < len(arr2):
-    #     merged_arr.append(arr2[j])
-    #     j += 1
-
-    #정말 생각지도 못한 방식이군! extend메소드로 그냥 i번째부터 가져다가 붙여버린다니.. 너무 똑똑한걸
-    merged_arr.extend(arr1[i:])
-    merged_arr.extend(arr2[j:])
-
-    return merged_arr
-
-def insert_arr(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
-
-print(merge_sort(a, b))
-print(insert_arr(c))
+arr = [1,2,3,4,5]
+pivot = arr[2]
+arr1 = [x for x in arr if x < pivot]
+print(arr1)
